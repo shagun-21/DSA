6b260cb5467a66f2d2068914729b4cf3f8bb82e7
shagun <shagun2001ch@gmail.com>
2021-12-12 17:23:23 +0530

    commited

---
+++ Sorting_algos/src/SelectionSort.java
@@ -0,0 +1,23 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,6,2,1,3};
+        int n= arr.length;
+        for(int i=0;i<n;i++){
+            int min_i=i;
+            for(int j=i+1;j<n;j++){
+                if (arr[j]<arr[min_i]){
+                    min_i=j;
+                    //swap
+                    int temp=arr[min_i];
+                    arr[min_i]=arr[i];
+                    arr[i]=temp;
+
+                }
+            }
+        }
+        //printing unsorted array
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+",");
+        }
+    }
+}
